9f39e0aae71526ac78723c162860dceff2e15749
Hamaiz <hamaiz5502@gamil.com>
2020-02-07 20:18:15 +0500

    Basics

---
+++ server.js
@@ -0,0 +1,30 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config()
+}
+
+//* Require
+const express = require("express");
+const app = express()
+const expressLayouts = require("express-ejs-layouts")
+
+//* Routes
+const indexRouter = require("./routes/home")
+
+
+//* MiddleWares
+app.set("view engine", "ejs")
+app.set("views", __dirname + "/views")
+app.set("layout", "layouts/layout")
+app.use(expressLayouts)
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: false }))
+
+//*Routes
+app.use("/home", indexRouter)
+
+
+app.get("/", (req, res) => {
+    res.send("This is the login page")
+})
+
+app.listen(process.env.PORT || 3000, () => console.log("Server has started..."))
