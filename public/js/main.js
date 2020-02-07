97b1955b0b856350792eac5d1fe931873750c029
Hamaiz <hamaiz5502@gamil.com>
2020-02-08 01:04:03 +0500

    Header Updtes

---
+++ public/js/main.js
@@ -0,0 +1,29 @@
+//* Scrolling
+window.onscroll = getScrollPosition;
+
+function getScrollPosition() {
+    const scrollObject = {
+        y: window.pageYOffset
+    }
+    const mainMenu = document.querySelector(".main_menu")
+    if (scrollObject.y < 50) {
+        mainMenu.classList.remove("menu_fixed")
+    } else {
+        mainMenu.classList.add("menu_fixed")
+    }
+}
+
+//Search Toggle
+const searchInputBox = document.getElementById("search_input_box")
+const searchOne = document.getElementById("search_1")
+const closeSearch = document.getElementById("close_search");
+
+searchInputBox.style.display = "none"
+
+searchOne.addEventListener("click", () => {
+    searchInputBox.style.display = searchInputBox.style.display === "none" ? "" : "none"
+})
+closeSearch.addEventListener("click", (e) => {
+    e.preventDefault()
+    searchInputBox.style.display = "none"
+})
