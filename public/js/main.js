dbdc47e40c470e2689423a37e2c3ef48a284d5b3
Hamaiz <hamaiz5502@gamil.com>
2020-02-08 23:33:19 +0500

    Footer Addition

--- public/js/main.js
+++ public/js/main.js
@@ -2,10 +2,10 @@
 window.onscroll = getScrollPosition;
 
 function getScrollPosition() {
-    const scrollObject = {
+    var scrollObject = {
         y: window.pageYOffset
     }
-    const mainMenu = document.querySelector(".main_menu")
+    var mainMenu = document.querySelector(".main_menu")
     if (scrollObject.y < 50) {
         mainMenu.classList.remove("menu_fixed")
     } else {
@@ -30,27 +30,39 @@ function getScrollPosition() {
 //     })
 // }(jQuery))
 
+$(document).ready(function () {
+    $("#search_input_box").hide();
+    $("#search_1").on("click", function () {
+        $("#search_input_box").slideToggle();
+        $("#search_input").focus();
+    });
+    $("#close_search").on("click", function (e) {
+        e.preventDefault()
+        $("#search_input_box").slideUp(500)
+    })
+}(jQuery));
+
 //? Mycode
-const searchInputBox = document.getElementById("search_input_box")
-const searchInput = document.getElementById("search_input")
-const searchOne = document.getElementById("search_1")
-const closeSearch = document.getElementById("close_search");
+// const searchInputBox = document.getElementById("search_input_box")
+// const searchInput = document.getElementById("search_input")
+// const searchOne = document.getElementById("search_1")
+// const closeSearch = document.getElementById("close_search");
 
-searchInputBox.style.display = "none"
-searchOne.addEventListener("click", () => {
-    searchInputBox.style.display = searchInputBox.style.display === "none" ? "" : "none"
-})
+// searchInputBox.style.display = "none"
+// searchOne.addEventListener("click", () => {
+//     searchInputBox.style.display = searchInputBox.style.display === "none" ? "" : "none"
+// })
 
-closeSearch.addEventListener("click", (e) => {
-    e.preventDefault()
-    searchInputBox.style.display = "none"
-})
+// closeSearch.addEventListener("click", (e) => {
+//     e.preventDefault()
+//     searchInputBox.style.display = "none"
+// })
 
 
 
 // *Hamburger Change
-const hamburger = document.getElementById("hamburger")
-const barsMobile = document.getElementById("bars-mobile");
+var hamburger = document.getElementById("hamburger")
+var barsMobile = document.getElementById("bars-mobile");
 
 
 hamburger.addEventListener("click", () => {
