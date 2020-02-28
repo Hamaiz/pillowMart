c2e474e8d25c32504fac4655a016b8c3c17df377
Hamaiz <hamaiz5502@gamil.com>
2020-02-28 23:22:30 +0500

    email verification system

--- routes/login.js
+++ routes/login.js
@@ -2,8 +2,10 @@ const express = require("express")
 const router = express.Router()
 const bcrypt = require("bcryptjs")
 const passport = require("passport")
-const nodemailer = require('nodemailer')
+// const nodemailer = require('nodemailer')
 const jwt = require('jsonwebtoken')
+const sgMail = require("@sendgrid/mail")
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 //Models
 const User = require("../models/User")
@@ -66,7 +68,7 @@ router.post("/register", forwardAuthenticated, (req, res) => {
                                         "success_msg",
                                         "You are now registered and can log in"
                                     )
-
+                                    //?Nodemailer
                                     // var auth = {
                                     // type: 'OAuth2',
                                     // user: process.env.GMAIL_USER,
@@ -75,16 +77,17 @@ router.post("/register", forwardAuthenticated, (req, res) => {
                                     // refreshToken: process.env.REFRESH_TOKEN,
                                     // }
 
-                                    const transporter = nodemailer.createTransport({
-                                        service: "gmail",
-                                        auth: {
-                                            user: process.env.GMAIL_USER,
-                                            pass: process.env.GMAIL_PASS
-                                        },
-                                        tls: {
-                                            rejectUnauthorized: false
-                                        }
-                                    })
+                                    // const transporter = nodemailer.createTransport({
+                                    // service: "gmail",
+                                    // auth: {
+                                    // user: process.env.GMAIL_USER,
+                                    // pass: process.env.GMAIL_PASS
+                                    // },
+                                    // tls: {
+                                    // rejectUnauthorized: false
+                                    // }
+                                    // })
+                                    //?
 
                                     jwt.sign(
                                         {
@@ -95,23 +98,142 @@ router.post("/register", forwardAuthenticated, (req, res) => {
                                             expiresIn: "1d"
                                         },
                                         (err, emailToken) => {
-                                            // const url = `http://localhost:3000/confirmation/${emailToken}`
+                                            // const url = `https://pillowmart.herokuapp.com/confirmation/${emailToken}`
+                                            const url = `http://localhost:3000/confirmation/${emailToken}`
 
-                                            const url = `https://pillowmart.herokuapp.com/confirmation/${emailToken}`
 
-                                            transporter.sendMail({
-                                                from: '"PillowMart Verification" <workingeveryday2@gmail.com>',
+                                            //?Send Grid
+                                            const msg = {
                                                 to: email,
-                                                subject: "Confirm Email",
-                                                html: `Please click this email to confirm your email: <a href="${url}">${url}</a>`
-                                            },
-                                                (err, info) => {
-                                                    if (err)
-                                                        console.error(err);
-                                                    else
-                                                        console.log(info);
-                                                }
-                                            )
+                                                from: 'PillowMart Verification <pillow@mart.co>',
+                                                subject: "Confirmation Email",
+                                                // html: `Please click this to confirm your email: <a href="${url}">${url}</a>`
+                                                html:
+                                                    `
+                                                <table width="100%" border="0" cellspacing="0" cellpadding="0"
+                                                style="width:100%!important;line-height: 1.4;color: #839197;">
+                                                <tr>
+                                                    <td align="center">
+                                                        <table width="600" border="0" cellspacing="0" cellpadding="40"
+                                                            style="border:1px solid #eaeaea;border-radius:5px;margin:40px 0">
+                                                            <tr>
+                                                                <td align="center">
+                                                                    <table width="100%" cellpadding="0" cellspacing="0">
+                                                                        <tr>
+                                                                            <td width="100%">
+                                                                                <table align="center" width="570" cellpadding="0" cellspacing="0" border="0">
+                                                                                    <tr>
+                                                                                        <td style="padding:10px 35px;">
+                                                                                            <h1
+                                                                                                style="color: #8c5a89;text-align: center;font-size: 20px;margin-bottom: 20px;">
+                                                                                                Verify
+                                                                                                your
+                                                                                                email
+                                                                                                to login into
+                                                                                                <span style="color: #5d3c5b;font-size: 23px">Pillow
+                                                                                                    Mart</span>
+                                                                                            </h1>
+                                                                                            <div style="color: #839197; margin-bottom: 10px;">Hello <strong
+                                                                                                    style="color: #5d3c5b;">${name}</strong>,
+                                                                                            </div>
+                                                                                            <p>Thanks for signing up for
+                                                                                                <span style=" font-size: 18px;color: #5d3c5b; font-weight:
+                                                                                                bold;">Pillow
+                                                                                                    Mart</span>.
+                                                                                                We're excited to have you as user. To complete the login
+                                                                                                process,
+                                                                                                please
+                                                                                                click the
+                                                                                                button below:
+                                                                                            </p>
+                                                                                            <table style=" margin: 30px auto;" align="center" width="100%"
+                                                                                                cellpadding="0" cellspacing="0">
+                                                                                                <tr>
+                                                                                                    <td align="center">
+                                                                                                        <div>
+                                                                                                            <a href="${url}"
+                                                                                                                style="display: inline-block;width: 200px;background-color: #8c5a89;border-radius: 3px;color: #ffffff;font-size: 15px;line-height: 45px;text-align: center;text-decoration: none;-webkit-text-size-adjust: none;mso-hide: all;margin-top: 20px;">
+                                                                                                                Verify Email</a>
+                                                                                                        </div>
+                                                                                                    </td>
+                                                                                                </tr>
+                                                                                            </table>
+                                                                                            <p style="text-align: center; padding-top: 20px;">
+                                                                                                Or copy and paste this URL into a new tab of your browser:
+                                                                                            </p>
+                                                                                            <div
+                                                                                                style="word-break: break-all;margin:30px 15px 50px;text-align: center;">
+                                                                                                <a href="${url}"style="text-align:center;color:#3899fa;text-decoration:none;font-size: 15px;">
+                                                                                                    ${url}
+                                                                                                </a>
+                                                                                            </div>
+                                        
+                                                                                            <p>Thanks,<br>Pillowmart Team</p>
+                                                                                            <table
+                                                                                                style="margin-top: 25px;padding-top: 20px;border-top: 1px solid #E7EAEC;">
+                                                                                                <tr>
+                                                                                                    <td>
+                                                                                                        <p class="sub">If you didn't attempt to log in but
+                                                                                                            received
+                                                                                                            this
+                                                                                                            email, or if the location doesn't match, please
+                                                                                                            ignore
+                                                                                                            this
+                                                                                                            email. If you are concerned about your account's
+                                                                                                            safety,
+                                                                                                            please
+                                                                                                            reply to this email to get in touch with us.
+                                                                                                        </p>
+                                                                                                    </td>
+                                                                                                </tr>
+                                                                                            </table>
+                                        
+                                                                                            <p style="font-size: 15px;text-align: center;">
+                                                                                                Pillowmart, Inc.
+                                                                                                <br>&copy; Copyrights reserved
+                                                                                            </p>
+                                                                                        </td>
+                                                                                    </tr>
+                                                                                </table>
+                                                                            </td>
+                                                                        </tr>
+                                                                    </table>
+                                                                </td>
+                                                            </tr>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                            `
+
+                                            }
+                                            sgMail.send(msg)
+                                            // (async () => {
+                                            // try {
+                                            // await sgMail.send(msg)
+                                            // } catch (error) {
+                                            // console.error(error.toString())
+                                            // }
+                                            // })
+
+
+
+                                            //?Nodemailer
+
+                                            // transporter.sendMail({
+                                            // from: '"PillowMart Verification" <no-reply@pillow.mart>',
+                                            // to: email,
+                                            // subject: "Confirm Email",
+                                            // html: `Please click this email to confirm your email: <a href="${url}">${url}</a>`
+                                            // },
+                                            // (err, info) => {
+                                            // if (err)
+                                            //    console.error(err);
+                                            // else
+                                            // console.log(info);
+                                            // }
+                                            // )
+                                            //?
                                         })
 
                                     res.redirect("confirm")
