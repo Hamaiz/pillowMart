ed0043abae9aafc1e0504e11d6e6ef99e2a79c20
Hamaiz <hamaiz5502@gamil.com>
2020-05-11 19:21:45 +0500

    Error Fix

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(express.static("public"))
 nm_dependencies.forEach(dep => {
     app.use(`/${dep}`, express.static(path.resolve(`node_modules/${dep}/`)))
 })
-// app.use(sslRedirect())
+app.use(sslRedirect())
 app.use(fileUpload())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -131,7 +131,4 @@ db.once("open", _ => console.log("Connected to Mongoose"))
 
 
 //*=============Port==============*//
-// app.listen(process.env.PORT || 3000, () => console.log("Server has started..."))
-app.listen(3000, '0.0.0.0', function () {
-    console.log("Server has started");
-});
+app.listen(process.env.PORT || 3000, () => console.log("Server has started..."))
